df1fdd23b220263f9b1bdd50164aa5935602c02e
Malik Al-Ansi <imalikril@gmail.com>
2024-11-19 15:08:36 +0100

    ATM program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace EATMNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM atm = new ATM();
+            bool huvudMeny = true;
+
+            while (huvudMeny)
+            {
+            Console.Clear();
+            Console.WriteLine("*********************************");
+            Console.WriteLine("Välkommen till E-ATM");
+            Console.WriteLine("*********************************"); 
+            Console.WriteLine("Välj ett val nedan:");
+            Console.WriteLine("1. Registrering kort ");
+            Console.WriteLine("2. Visa penger");
+            Console.WriteLine("3. instätta penger");
+            Console.WriteLine("4. utsätta penger");
+            Console.WriteLine("5. Visa transaktionshistorik");
+            Console.WriteLine("Q. Avslutas programmet");
+            
+            string val = Utilities.ValidateString();
+            switch (val)
+            {
+                case "1":
+                    atm.InputCardDetails();
+                    break;
+
+                case "2":
+                    atm.ShowBalance();
+                    break;
+
+                case "3":
+                    atm.MakeDeposit();
+                    break;
+
+                case "4":
+                    atm.MakeWithdrawal();
+                    break;
+
+
+                 case "5":
+                    atm.ShowTransactionHistory();
+                    break;
+
+               
+                
+                case "Q":
+                    Console.WriteLine("Programmet avslutas...");
+                    Thread.Sleep(2000);
+                    huvudMeny = false;
+                    break;
+                default:
+                    Console.WriteLine("Ogiltlig inmatning! ");
+                    break;
+    }
+            }
+       }
+    }
+
+}
